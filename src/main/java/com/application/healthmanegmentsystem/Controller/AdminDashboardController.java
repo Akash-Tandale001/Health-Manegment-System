a70e2ce78f22448ae8afb73125aa8d7d372217b2
Akash-Tandale001 <ak.tandale123@gmail.com>
2022-11-18 11:14:18 +0530

    designed admin page

--- src/main/java/com/application/healthmanegmentsystem/Controller/AdminDashboardController.java
+++ src/main/java/com/application/healthmanegmentsystem/Controller/AdminDashboardController.java
@@ -1,14 +1,24 @@
 package com.application.healthmanegmentsystem.Controller;
 
+import com.application.healthmanegmentsystem.Entity.UserInfo;
+import com.application.healthmanegmentsystem.Services.ServicesImplementation.PatientServicesImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/admin")
 public class AdminDashboardController {
+    @Autowired
+    private PatientServicesImpl patientServices;
     @GetMapping()
-    public String getAdminDashboard(){
+    public String getAdminDashboard(Model model){
+        List<UserInfo> userInfoList = patientServices.getAllUser();
+        model.addAttribute("patients",userInfoList);
         return "/Auth/AdminDashboard";
     }
     @GetMapping("/ambulanceRequest")
--- src/main/java/com/application/healthmanegmentsystem/Services/PatientServices.java
+++ src/main/java/com/application/healthmanegmentsystem/Services/PatientServices.java
@@ -2,6 +2,9 @@ package com.application.healthmanegmentsystem.Services;
 
 import com.application.healthmanegmentsystem.Entity.UserInfo;
 
+import java.util.List;
+
 public interface PatientServices {
     String processRegistration(UserInfo userPatient);
+    List<UserInfo> getAllUser();
 }
