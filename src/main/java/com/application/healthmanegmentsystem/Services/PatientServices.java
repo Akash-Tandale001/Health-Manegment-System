4d1a2ba54c5ad21f1ecfec5a97d83071ce4d1eef
Akash-Tandale001 <ak.tandale123@gmail.com>
2022-11-29 15:13:42 +0530

    services implementation

--- src/main/java/com/application/healthmanegmentsystem/Services/PatientServices.java
+++ src/main/java/com/application/healthmanegmentsystem/Services/PatientServices.java
@@ -1,7 +1,6 @@
 package com.application.healthmanegmentsystem.Services;
 
-import com.application.healthmanegmentsystem.Entity.Appointment;
-import com.application.healthmanegmentsystem.Entity.UserInfo;
+import com.application.healthmanegmentsystem.Entity.*;
 
 import java.util.List;
 import java.util.Set;
@@ -12,9 +11,36 @@ public interface PatientServices {
     UserInfo getLogedInUserDetailss(String Username);
 
     List<Appointment> getAllAppointment();
+    List<AmbulanceService> getAllAmbulanceService();
+    List<BedFacilityService> getAllBedFacilityService();
+    List<FreeCheckupService> getAllFreeCheckupService();
+    List<MedicineService> getAllMedicineService();
+    List<TotalcareService> getAllTotalcareService();
     void updateProfile(UserInfo userInfo);
 
     void saveAppoinment(Appointment appointment,String username);
+    void saveAmbulanceService(AmbulanceService ambulanceService, String username);
+    void saveBedFacilityService(BedFacilityService bedFacilityService, String username);
+    void saveFreeCheckupService(FreeCheckupService freeCheckupService, String username);
+    void saveMedicineService(MedicineService medicineService,String username);
+    void saveTotalcareService(TotalcareService totalcareService,String username);
+
+
     void acceptAppointmentById(Long id);
     void rejectAppointmentById(Long id);
+    void acceptBedFacilityServiceById(Long id);
+    void rejectBedFacilityServiceById(Long id);
+    void acceptAmbulanceServiceById(Long id);
+    void rejectAmbulanceServiceById(Long id);
+
+    void acceptFreeCheckupServiceById(Long id);
+    void rejectFreeCheckupServiceById(Long id);
+
+    void acceptMedicineServiceById(Long id);
+    void rejectMedicineServiceById(Long id);
+
+    void acceptTotalcareServiceById(Long id);
+    void rejectTotalcareServiceById(Long id);
+
+
 }
